6f833f3fbbc597a1f2dc769845432b8b3b09a0a4
Chrisys93 <nicolaescu3@yahoo.com>
2018-10-19 16:06:47 +0100

    Reworked matlab plotting

--- one_1.4.1/reports/Untitled.m
+++ one_1.4.1/reports/Untitled.m
@@ -1,7 +1,7 @@
 clear
 R = dlmread('default_scenario_RepoMessageLocationReport.txt', ' ', 0, 1);
 M = dlmread('default_scenario_MobileMessageLocationReport.txt', ' ', 0, 1);
-S = dlmread('default_scenario_RepoStoredMessageLocationReport.txt', ' ', 0, 1);
+S = dlmread('default_scenario_RepoStoredMessageLocationReport.txt', ' ', 1, 1);
 [r1, c1] = size(R);
 [r2, c2] = size(M);
 [r3, c3] = size(S);
@@ -19,10 +19,16 @@ for i = 1:r2
 end
 maxval2 = max(maxstor2);
 
+s = 10000;
 for i = 1:r3
     maxstor3(i) = max(S(i, :));
+    if maxstor3(i) >= 10000 && i < s
+        figure
+        bar(S(i, :));
+        s = i;
+    end
 end
-maxval2 = max(maxstor3);
+maxval3 = max(maxstor3);
 
 figure
 bar(maxstor1);
@@ -37,7 +43,7 @@ xlabel('Time(s)')
 ylabel('Space used(B)')
 
 figure
-bar(maxstor3);
+bar(maxstor3, 0.5);
 title('Bar plot of Repos Stored Nodes Storage Usage')
 xlabel('Time(s)') 
 ylabel('Space used(B)')
@@ -49,5 +55,11 @@ ylabel('Space used(B)')
 %bar3(R);
 
 %figure
-%bar3(S);
+%bar3(S, 0.5);
+
+%figure
+%bmesh(S);
+
+figure
+stem3(S, ':.');
 
