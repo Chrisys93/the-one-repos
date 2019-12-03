1bfb986aa3c40465da771189b739df8c443866c1
Chrisys93 <nicolaescu3@yahoo.com>
2019-12-03 12:06:51 +0000

    Changed Proc_UnProc_all in both cloud and comp

--- one-cloud/Proc_UnProc_all.m
+++ one-cloud/Proc_UnProc_all.m
@@ -421,66 +421,233 @@ bar([mean(oproc(:,1)), mean(ouproc(:,1)), mean(oproc(:,2)), mean(ouproc(:,2)), m
 % title('Processing threads','fontsize',16)
 xlabel('Scenario Number','fontsize',12) 
 ylabel('Processed/UnProcessed Messages BW (B/s)','fontsize',12)
-%TSDS:
-% Modify this:
-% lgd1 =legend('1: f; 1.1; 100; 3:1 \newline2: f; 3; 100; 4:1; \newline3: t; 4; 100; 4:1; \newline4: t; 2; 100; 1:2', ...
-%             '1: f; 1.3; 100; 3:1 \newline2: f; 3.2; 100; 4:1; \newline3: t; 2.2; 500; 4:1;  \newline4: t; 2; 500; 2:2', ...
-%             '1: f; 2; 100; 3:1 \newline2: f; 50; 100; 4:1; \newline3: f; 3; 100 4:1; \newline4: f; 2; 100; 4:2', ...
-%             '1: f; 1.6; 300; 3:1 \newline2: f; 3; 200; 4:1; \newline3: f; 4; 300 4:1; \newline4: f; 2; 300; 5:2', ...
-%             '1: f; 2.2; 600; 3:1 \newline2: f; 3.2; 300; 4:1; \newline3: f; 2.2; 600 4:1; \newline4: f; 2; 600; 3:1', ...
-%             '1: f; 2; 900; 4:1 \newline2: f; 50; 600; 4:1; \newline3: f; 3; 900 4:1; \newline4: f; 2; 900; 4:1', 'Location', 'southoutside');
-% title(lgd1, 'Scenario Number: Storage Mode; Processing Delay; proc Freshness Period; proc Shelf-Life; non-proc Shelf-Life; Compress:Delete Ratio; Non-Proc:Proc Ratio');
-% lgd1.NumColumns = 6;
+OPF1 = dlmread('officeIoT/RPFR1', ' ', 0, 1);
+HPF1 = dlmread('homeIoT/RPFR1', ' ', 0, 1);
+BPF1 = dlmread('buses/RPFR1', ' ', 0, 1);
+PPF1 = dlmread('non-proc_proc/RPFR1', ' ', 0, 1);
 
 
-% procApp2.delay = 0.1
-% procApp2.freshPer = [1; 1; 1; 1.5; 2; 3; 1]
-% procApp2.procShelf = [1.1; 1.3; 2; 1.6; 2.2; 3.3; 2]
-% procApp2.nonprocShelf = [100; 100; 100; 200; 300; 600]
-% procApp2.destinationName = r
-% procApp2.procSize = 50k
-% # Stored message ratio to processing message (no. of messages to be stored vs. no. of messages to be processed)
-% procApp2.compressRate = 1,4
-% procApp2.passiveRate = [3,1; 3,1; 3,1; 3,1; 3,1; 4,1]
+OPF2 = dlmread('officeIoT/RPFR2', ' ', 0, 1);
+HPF2 = dlmread('homeIoT/RPFR2', ' ', 0, 1);
+BPF2 = dlmread('buses/RPFR2', ' ', 0, 1);
+PPF2 = dlmread('non-proc_proc/RPFR2', ' ', 0, 1);
 
 
-% procApp2.delay = [0.2; 0.2; 0.2; 0.1; 0.1; 0.1]
-% procApp2.freshPer = [3; 3; 3; 2; 2; 2]
-% procApp2.procShelf = [3; 3.2; 50]
-% procApp2.nonprocShelf = [100; 100; 100; 200; 300; 600]
-% procApp2.destinationName = r
-% procApp2.procSize = 1M
-% # Stored message ratio to processing message (no. of messages to be stored vs. no. of messages to be processed)
-% procApp2.compressRate = [2,1; 2,1; 2,2; 2,2; 1,2; 1,2]
-% procApp2.passiveRate = 4,1
+OPF3 = dlmread('officeIoT/RPFR3', ' ', 0, 1);
+HPF3 = dlmread('homeIoT/RPFR3', ' ', 0, 1);
+BPF3 = dlmread('buses/RPFR3', ' ', 0, 1);
+PPF3 = dlmread('non-proc_proc/RPFR3', ' ', 0, 1);
 
 
-% procApp.storageMode = [true; true; false; false; false; false]
-% procApp.maxStorTime = [3000; 3000]
-% 
-% 
-% procApp3.interval = 0.7
-% procApp3.delay = [0.3; 0.3; 0.3; 0.3; 0.1; 0.1]
-% procApp3.freshPer = [3; 2; 2]
-% procApp3.procShelf = [4; 2.2; 3]
-% procApp3.nonprocShelf = [100; 500; 100; 300; 600; 900]
-% procApp3.destinationName = r
-% procApp3.procSize = 1M
-% # Stored message ratio to processing message (no. of messages to be stored vs. no. of messages to be processed)
-% procApp3.compressRate = 3,1
-% procApp3.passiveRate = [4,1]
-% procApp3.passive = false
-% 
-% 
-% procApp2.delay = 0.1
-% procApp2.freshPer = 1
-% procApp2.procShelf = 2
-% procApp3.nonprocShelf = [100; 500; 100; 300; 600; 900]
-% procApp2.destinationName = r
-% procApp2.procSize = 500k
-% # Stored message ratio to processing message (no. of messages to be stored vs. no. of messages to be processed)
-% procApp2.compressRate = 2,2
-% procApp2.passiveRate = [4,1; 3,1; 5,2; 4,2; 2,2; 1,2]
-% procApp2.passive = false
+OPF4 = dlmread('officeIoT/RPFR4', ' ', 0, 1);
+HPF4 = dlmread('homeIoT/RPFR4', ' ', 0, 1);
+BPF4 = dlmread('buses/RPFR4', ' ', 0, 1);
+PPF4 = dlmread('non-proc_proc/RPFR4', ' ', 0, 1);
+
+
+OPF5 = dlmread('officeIoT/RPFR5', ' ', 0, 1);
+HPF5 = dlmread('homeIoT/RPFR5', ' ', 0, 1);
+BPF5 = dlmread('buses/RPFR5', ' ', 0, 1);
+PPF5 = dlmread('non-proc_proc/RPFR5', ' ', 0, 1);
+
+
+OPF6 = dlmread('officeIoT/RPFR6', ' ', 0, 1);
+HPF6 = dlmread('homeIoT/RPFR6', ' ', 0, 1);
+BPF6 = dlmread('buses/RPFR6', ' ', 0, 1);
+PPF6 = dlmread('non-proc_proc/RPFR6', ' ', 0, 1);
+
+
+[r3, c3] = size(OPF1);
+
+for repo = 1:c3
+    ofresh_perc(repo, 1) = OPF1(1, repo)/sum(OPF1(:, repo));
+    if (isnan(OPF1(1, repo)/sum(OPF1(:, repo))))
+        ofresh_perc(repo, 1) = 0;
+    end
+    ofresh_perc(repo, 2) = OPF2(1, repo)/sum(OPF2(:, repo));
+    if (isnan(OPF2(1, repo)/sum(OPF2(:, repo))))
+        ofresh_perc(repo, 2) = 0;
+    end
+    ofresh_perc(repo, 3) = OPF3(1, repo)/sum(OPF3(:, repo));
+    if (isnan(OPF3(1, repo)/sum(OPF3(:, repo))))
+        ofresh_perc(repo, 3) = 0;
+    end
+    ofresh_perc(repo, 4) = OPF4(1, repo)/sum(OPF4(:, repo));
+    if (isnan(OPF4(1, repo)/sum(OPF4(:, repo))))
+        ofresh_perc(repo, 4) = 0;
+    end
+    ofresh_perc(repo, 5) = OPF5(1, repo)/sum(OPF5(:, repo));
+    if (isnan(OPF5(1, repo)/sum(OPF5(:, repo))))
+        ofresh_perc(repo, 5) = 0;
+    end
+    ofresh_perc(repo, 6) =  OPF6(1, repo)/sum(OPF6(:, repo));
+    if (isnan(OPF6(1, repo)/sum(OPF6(:, repo))))
+        ofresh_perc(repo, 6) = 0;
+    end
+    hfresh_perc(repo, 1) = HPF1(1, repo)/sum(HPF1(:, repo));
+    if (isnan(HPF1(1, repo)/sum(HPF1(:, repo))))
+        hfresh_perc(repo, 1) = 0;
+    end
+    hfresh_perc(repo, 2) = HPF2(1, repo)/sum(HPF2(:, repo));
+    if (isnan(HPF2(1, repo)/sum(HPF2(:, repo))))
+        hfresh_perc(repo, 2) = 0;
+    end
+    hfresh_perc(repo, 3) = HPF3(1, repo)/sum(HPF3(:, repo));
+    if (isnan(HPF3(1, repo)/sum(HPF3(:, repo))))
+        hfresh_perc(repo, 3) = 0;
+    end
+    hfresh_perc(repo, 4) = HPF4(1, repo)/sum(HPF4(:, repo));
+    if (isnan(HPF4(1, repo)/sum(HPF4(:, repo))))
+        hfresh_perc(repo, 4) = 0;
+    end
+    hfresh_perc(repo, 5) = HPF5(1, repo)/sum(HPF5(:, repo));
+    if (isnan(HPF5(1, repo)/sum(HPF5(:, repo))))
+        hfresh_perc(repo, 5) = 0;
+    end
+    hfresh_perc(repo, 6) =  HPF6(1, repo)/sum(HPF6(:, repo));
+    if (isnan(HPF6(1, repo)/sum(HPF6(:, repo))))
+        hfresh_perc(repo, 6) = 0;
+    end
+    bfresh_perc(repo, 1) = BPF1(1, repo)/sum(BPF1(:, repo));
+    if (isnan(BPF1(1, repo)/sum(BPF1(:, repo))))
+        bfresh_perc(repo, 1) = 0;
+    end
+    bfresh_perc(repo, 2) = BPF2(1, repo)/sum(BPF2(:, repo));
+    if (isnan(BPF2(1, repo)/sum(BPF2(:, repo))))
+        bfresh_perc(repo, 2) = 0;
+    end
+    bfresh_perc(repo, 3) = BPF3(1, repo)/sum(BPF3(:, repo));
+    if (isnan(BPF3(1, repo)/sum(BPF3(:, repo))))
+        bfresh_perc(repo, 3) = 0;
+    end
+    bfresh_perc(repo, 4) = BPF4(1, repo)/sum(BPF4(:, repo));
+    if (isnan(BPF4(1, repo)/sum(BPF4(:, repo))))
+        bfresh_perc(repo, 4) = 0;
+    end
+    bfresh_perc(repo, 5) = BPF5(1, repo)/sum(BPF5(:, repo));
+    if (isnan(BPF5(1, repo)/sum(BPF5(:, repo))))
+        bfresh_perc(repo, 5) = 0;
+    end
+    bfresh_perc(repo, 6) =  BPF6(1, repo)/sum(BPF6(:, repo));
+    if (isnan(BPF6(1, repo)/sum(BPF6(:, repo))))
+        bfresh_perc(repo, 6) = 0;
+    end
+    pfresh_perc(repo, 1) = PPF1(1, repo)/sum(PPF1(:, repo));
+    if (isnan(PPF1(1, repo)/sum(PPF1(:, repo))))
+        pfresh_perc(repo, 1) = 0;
+    end
+    pfresh_perc(repo, 2) = PPF2(1, repo)/sum(PPF2(:, repo));
+    if (isnan(PPF2(1, repo)/sum(PPF2(:, repo))))
+        pfresh_perc(repo, 2) = 0;
+    end
+    pfresh_perc(repo, 3) = PPF3(1, repo)/sum(PPF3(:, repo));
+    if (isnan(PPF3(1, repo)/sum(PPF3(:, repo))))
+        pfresh_perc(repo, 3) = 0;
+    end
+    pfresh_perc(repo, 4) = PPF4(1, repo)/sum(PPF4(:, repo));
+    if (isnan(PPF4(1, repo)/sum(PPF4(:, repo))))
+        pfresh_perc(repo, 4) = 0;
+    end
+    pfresh_perc(repo, 5) = PPF5(1, repo)/sum(PPF5(:, repo));
+    if (isnan(PPF5(1, repo)/sum(PPF5(:, repo))))
+        pfresh_perc(repo, 5) = 0;
+    end
+    pfresh_perc(repo, 6) =  PPF6(1, repo)/sum(PPF6(:, repo));
+    if (isnan(PPF6(1, repo)/sum(PPF6(:, repo))))
+        pfresh_perc(repo, 6) = 0;
+    end
+end
+
+
+figure
+
+% subplot(2,1,1);
+% yyaxis left
+bar_handle = bar([ofresh_perc(:,1), ofresh_perc(:,2), ofresh_perc(:,3), ofresh_perc(:,4), ofresh_perc(:,5), ofresh_perc(:,6)]);
+% title('Processing threads','fontsize',16)
+xlabel('Repository number','fontsize',12) 
+ylabel('Processing Message Freshness (100%)','fontsize',12)
+ylim([0 1]);
+% xlim([17 48]);
+set(bar_handle(1),'FaceColor',[0,0.5,1])
+set(bar_handle(2),'FaceColor',[0,1,0])
+
+
+
+
+figure
+
+% subplot(2,1,1);
+% yyaxis left
+bar([mean(nonzeros(ofresh_perc(:,1))), mean(nonzeros(ofresh_perc(:,2))), mean(nonzeros(ofresh_perc(:,3))), mean(nonzeros(ofresh_perc(:,4))), mean(nonzeros(ofresh_perc(:,5))), mean(nonzeros(ofresh_perc(:,6)));
+    mean(nonzeros(hfresh_perc(:,1))), mean(nonzeros(hfresh_perc(:,2))), mean(nonzeros(hfresh_perc(:,3))), mean(nonzeros(hfresh_perc(:,4))), mean(nonzeros(hfresh_perc(:,5))), mean(nonzeros(hfresh_perc(:,6)));
+    mean(nonzeros(bfresh_perc(:,1))), mean(nonzeros(bfresh_perc(:,2))), mean(nonzeros(bfresh_perc(:,3))), mean(nonzeros(bfresh_perc(:,4))), mean(nonzeros(bfresh_perc(:,5))), mean(nonzeros(bfresh_perc(:,6)));
+    mean(nonzeros(pfresh_perc(:,1))), mean(nonzeros(pfresh_perc(:,2))), mean(nonzeros(pfresh_perc(:,3))), mean(nonzeros(pfresh_perc(:,4))), mean(nonzeros(pfresh_perc(:,5))), mean(nonzeros(pfresh_perc(:,6)))]);
+% title('Processing threads','fontsize',16)
+xlabel('Scenario Number','fontsize',12) 
+ylabel('Processing Message Freshness (100%)','fontsize',12)
+ylim([0 1]);
+
+
+
+figure
+
+% yyaxis left
+y = [mean(nonzeros(oproc_up(:,1))), mean(nonzeros(oproc_up(:,2))), mean(nonzeros(oproc_up(:,3))), mean(nonzeros(oproc_up(:,4))), mean(nonzeros(oproc_up(:,5))), mean(nonzeros(oproc_up(:,6)));
+    mean(nonzeros(ofresh_perc(:,1))), mean(nonzeros(ofresh_perc(:,2))), mean(nonzeros(ofresh_perc(:,3))), mean(nonzeros(ofresh_perc(:,4))), mean(nonzeros(ofresh_perc(:,5))), mean(nonzeros(ofresh_perc(:,6)));
+    mean(nonzeros(hproc_up(:,1))), mean(nonzeros(hproc_up(:,2))), mean(nonzeros(hproc_up(:,3))), mean(nonzeros(hproc_up(:,4))), mean(nonzeros(hproc_up(:,5))), mean(nonzeros(hproc_up(:,6)));
+    mean(nonzeros(hfresh_perc(:,1))), mean(nonzeros(hfresh_perc(:,2))), mean(nonzeros(hfresh_perc(:,3))), mean(nonzeros(hfresh_perc(:,4))), mean(nonzeros(hfresh_perc(:,5))), mean(nonzeros(hfresh_perc(:,6)));
+    mean(nonzeros(bproc_up(:,1))), mean(nonzeros(bproc_up(:,2))), mean(nonzeros(bproc_up(:,3))), mean(nonzeros(bproc_up(:,4))), mean(nonzeros(bproc_up(:,5))), mean(nonzeros(bproc_up(:,6)));
+    mean(nonzeros(bfresh_perc(:,1))), mean(nonzeros(bfresh_perc(:,2))), mean(nonzeros(bfresh_perc(:,3))), mean(nonzeros(bfresh_perc(:,4))), mean(nonzeros(bfresh_perc(:,5))), mean(nonzeros(bfresh_perc(:,6)));
+    mean(nonzeros(pproc_up(:,1))), mean(nonzeros(pproc_up(:,2))), mean(nonzeros(pproc_up(:,3))), mean(nonzeros(pproc_up(:,4))), mean(nonzeros(pproc_up(:,5))), mean(nonzeros(pproc_up(:,6)))
+    mean(nonzeros(pfresh_perc(:,1))), mean(nonzeros(pfresh_perc(:,2))), mean(nonzeros(pfresh_perc(:,3))), mean(nonzeros(pfresh_perc(:,4))), mean(nonzeros(pfresh_perc(:,5))), mean(nonzeros(pfresh_perc(:,6)))];
+bar(y,'group')
+set(gca,'xtick',1:8,'XTickLabel',{'Processed', 'Fresh', 'Processed', 'Fresh', 'Processed', 'Fresh', 'Processed', 'Fresh'}, 'fontsize', 10)
+% xt= [1-0.34, 1-0.2, 1-0.07, 1.07, 1.2, 1.34, 3-0.34, 3-0.2, 3-0.07, 3.07, 3.2, 3.34, 5-0.34, 5-0.2, 5-0.07, 5.07, 5.2, 5.34, 7-0.34, 7-0.2, 7-0.07, 7.07, 7.2, 7.34];
+% yt=[y(1,:), y(3,:), y(5,:), y(7,:)]+50;
+%xt=[1, 3, 5, 7]-0.25;
+%yt=[y(1,1); y(3,1); y(5,1); y(7,1)]+50;
+% ytxt=num2str([y(1, :)'; y(3, :)'; y(5, :)'; y(7, :)'],'%.1f');
+% text(xt,yt,ytxt,'rotation',90,'fontsize',8,'fontweight','bold')
+% title('Processing threads','fontsize',16)
+% xlabel('Scenario Number','fontsize',12) 
+ylabel('Non-processing Message Satisfied Upload (100%)','fontsize',12)
+
+groupX = [2 4 6 8]; %// central value of each group
+groupY = -0.1; %// vertical position of texts. Adjust as needed
+deltaY = .03; %// controls vertical compression of axis. Adjust as needed
+groupNames = {'Offices', 'Homes', 'Buses' 'Cars'}; %// note different lengths to test centering
+for g = 1:numel(groupX)
+    h = text(groupX(g), groupY, groupNames(g), 'Fontsize', 13, 'Fontweight', 'bold');
+        %// create text for group with appropriate font size and weight
+    pos = get(h, 'Position');
+    ext = get(h, 'Extent');
+    pos(1) = pos(1) - ext(3)/2; %// horizontally correct position to make it centered
+    set(h, 'Position', pos); %// set corrected position for text
+end
+pos = get(gca, 'Position');
+pos(2) = pos(2) + deltaY; %// vertically compress axis to make room for texts
+set(gca, 'Position', pos); %/ set corrected position for axis
+
+
+% yyaxis right
+% y = [mean(nonzeros(oproc_up(:,1))), mean(nonzeros(oproc_up(:,2))), mean(nonzeros(oproc_up(:,3))), mean(nonzeros(oproc_up(:,4))), mean(nonzeros(oproc_up(:,5))), mean(nonzeros(oproc_up(:,6)));
+%     mean(nonzeros(ofresh_perc(:,1))), mean(nonzeros(ofresh_perc(:,2))), mean(nonzeros(ofresh_perc(:,3))), mean(nonzeros(ofresh_perc(:,4))), mean(nonzeros(ofresh_perc(:,5))), mean(nonzeros(ofresh_perc(:,6)));
+%     mean(nonzeros(hproc_up(:,1))), mean(nonzeros(hproc_up(:,2))), mean(nonzeros(hproc_up(:,3))), mean(nonzeros(hproc_up(:,4))), mean(nonzeros(hproc_up(:,5))), mean(nonzeros(hproc_up(:,6)));
+%     mean(nonzeros(hfresh_perc(:,1))), mean(nonzeros(hfresh_perc(:,2))), mean(nonzeros(hfresh_perc(:,3))), mean(nonzeros(hfresh_perc(:,4))), mean(nonzeros(hfresh_perc(:,5))), mean(nonzeros(hfresh_perc(:,6)));
+%     mean(nonzeros(bproc_up(:,1))), mean(nonzeros(bproc_up(:,2))), mean(nonzeros(bproc_up(:,3))), mean(nonzeros(bproc_up(:,4))), mean(nonzeros(bproc_up(:,5))), mean(nonzeros(bproc_up(:,6)));
+%     mean(nonzeros(bfresh_perc(:,1))), mean(nonzeros(bfresh_perc(:,2))), mean(nonzeros(bfresh_perc(:,3))), mean(nonzeros(bfresh_perc(:,4))), mean(nonzeros(bfresh_perc(:,5))), mean(nonzeros(bfresh_perc(:,6)));
+%     mean(nonzeros(pproc_up(:,1))), mean(nonzeros(pproc_up(:,2))), mean(nonzeros(pproc_up(:,3))), mean(nonzeros(pproc_up(:,4))), mean(nonzeros(pproc_up(:,5))), mean(nonzeros(pproc_up(:,6)))
+%     mean(nonzeros(pfresh_perc(:,1))), mean(nonzeros(pfresh_perc(:,2))), mean(nonzeros(pfresh_perc(:,3))), mean(nonzeros(pfresh_perc(:,4))), mean(nonzeros(pfresh_perc(:,5))), mean(nonzeros(pfresh_perc(:,6)))];
+% bar([1:8],[0,0,0,0,0,0; y(2, :); 0,0,0,0,0,0; y(4, :); 0,0,0,0,0,0; y(6, :); 0,0,0,0,0,0; y(8, :)],'group')
+% % set(gca,'xtick',1:8)
+% % xt= [2-0.34, 2-0.2, 2-0.07, 2.07, 2.2, 2.34, 4-0.34, 4-0.2, 4-0.07, 4.07, 4.2, 4.34, 6-0.34, 6-0.2, 6-0.07, 6.07, 6.2, 6.34, 8-0.34, 8-0.2, 8-0.07, 8.07, 8.2, 8.34];
+% % yt=[y(2,:), y(4,:), y(6,:), y(8,:)]-50;
+% %xt=[1, 3, 5, 7]-0.25;
+% %yt=[y(1,1); y(3,1); y(5,1); y(7,1)]+50;
+% % ytxt=num2str([y(2, :)'; y(4, :)'; y(6, :)'; y(8, :)'],'%.1f');
+% % text(xt,yt,ytxt,'rotation',90,'fontsize',8,'fontweight','bold')
+% % title('Processing threads','fontsize',16)
+% ylabel('Non-processing Message Satisfied Upload (100%)','fontsize',12)
 
 
