590bea131192326b3d7a92960788a4d58d310288
Chrisys93 <nicolaescu3@yahoo.com>
2019-12-06 00:26:03 +0000

    Added shelf-constant and varying freshness deadline scenarios

--- one-comp/Proc_UnProc_all.m
+++ one-comp/Proc_UnProc_all.m
@@ -117,8 +117,8 @@ for repo = 1:c3
     if (isnan(mean(HPB4(:, repo))/(mean(HUB4(:, repo))+mean(HPB4(:, repo)))))
         hproc_up(repo, 4) = 0;
     end
-    hproc_up(repo, 5) = mean(HPB6(:, repo))/(mean(HUB5(:, repo))+mean(HPB5(:, repo)));
-    if (isnan(mean(HPB6(:, repo))/(mean(HUB5(:, repo))+mean(HPB5(:, repo)))))
+    hproc_up(repo, 5) = mean(HPB5(:, repo))/(mean(HUB5(:, repo))+mean(HPB5(:, repo)));
+    if (isnan(mean(HPB5(:, repo))/(mean(HUB5(:, repo))+mean(HPB5(:, repo)))))
         hproc_up(repo, 5) = 0;
     end
     hproc_up(repo, 6) = mean(HPB6(:, repo))/(mean(HUB6(:, repo))+mean(HPB6(:, repo)));
@@ -172,6 +172,104 @@ for repo = 1:c3
     pproc_up(repo, 6) =  mean(PPB6(:, repo))/(mean(PUB6(:, repo))+mean(PPB6(:, repo)));
     if (isnan(mean(PPB6(:, repo))/(mean(PUB6(:, repo))+mean(PPB6(:, repo)))))
         pproc_up(repo, 6) = 0;
+    end
+    
+    
+    ouproc_up(repo, 1) = mean(OUB1(:, repo))/(mean(OUB1(:, repo))+mean(OPB1(:, repo)));
+    if (isnan(mean(OUB1(:, repo))/(mean(OUB1(:, repo))+mean(OPB1(:, repo)))))
+        ouproc_up(repo, 1) = 0;
+    end
+    ouproc_up(repo, 2) = mean(OUB2(:, repo))/(mean(OUB2(:, repo))+mean(OPB2(:, repo)));
+    if (isnan(mean(OUB2(:, repo))/(mean(OUB2(:, repo))+mean(OPB2(:, repo)))))
+        ouproc_up(repo, 2) = 0;
+    end
+    ouproc_up(repo, 3) = mean(OUB3(:, repo))/(mean(OUB3(:, repo))+mean(OPB3(:, repo)));
+    if (isnan(mean(OUB3(:, repo))/(mean(OUB3(:, repo))+mean(OPB3(:, repo)))))
+        ouproc_up(repo, 3) = 0;
+    end
+    ouproc_up(repo, 4) = mean(OUB4(:, repo))/(mean(OUB4(:, repo))+mean(OPB4(:, repo)));
+    if (isnan(mean(OUB4(:, repo))/(mean(OUB4(:, repo))+mean(OPB4(:, repo)))))
+        ouproc_up(repo, 4) = 0;
+    end
+    ouproc_up(repo, 5) = mean(OUB5(:, repo))/(mean(OUB5(:, repo))+mean(OPB5(:, repo)));
+    if (isnan(mean(OUB5(:, repo))/(mean(OUB5(:, repo))+mean(OPB5(:, repo)))))
+        ouproc_up(repo, 5) = 0;
+    end
+    ouproc_up(repo, 6) =  mean(OUB6(:, repo))/(mean(OUB6(:, repo))+mean(OPB6(:, repo)));
+    if (isnan(mean(OUB6(:, repo))/(mean(OUB6(:, repo))+mean(OPB6(:, repo)))))
+        ouproc_up(repo, 6) = 0;
+    end
+    huproc_up(repo, 1) = mean(HUB1(:, repo))/(mean(HUB1(:, repo))+mean(HPB1(:, repo)));
+    if (isnan(mean(HUB1(:, repo))/(mean(HUB1(:, repo))+mean(HPB1(:, repo)))))
+        huproc_up(repo, 1) = 0;
+    end
+    huproc_up(repo, 2) = mean(HUB2(:, repo))/(mean(HUB2(:, repo))+mean(HPB2(:, repo)));
+    if (isnan(mean(HUB2(:, repo))/(mean(HUB2(:, repo))+mean(HPB2(:, repo)))))
+        huproc_up(repo, 2) = 0;
+    end
+    huproc_up(repo, 3) = mean(HUB3(:, repo))/(mean(HUB3(:, repo))+mean(HPB3(:, repo)));
+    if (isnan(mean(HUB3(:, repo))/(mean(HUB3(:, repo))+mean(HPB3(:, repo)))))
+        huproc_up(repo, 3) = 0;
+    end
+    huproc_up(repo, 4) = mean(HUB4(:, repo))/(mean(HUB4(:, repo))+mean(HPB4(:, repo)));
+    if (isnan(mean(HUB4(:, repo))/(mean(HUB4(:, repo))+mean(HPB4(:, repo)))))
+        huproc_up(repo, 4) = 0;
+    end
+    huproc_up(repo, 5) = mean(HUB5(:, repo))/(mean(HUB5(:, repo))+mean(HPB5(:, repo)));
+    if (isnan(mean(HUB5(:, repo))/(mean(HUB5(:, repo))+mean(HPB5(:, repo)))))
+        huproc_up(repo, 5) = 0;
+    end
+    huproc_up(repo, 6) = mean(HUB6(:, repo))/(mean(HUB6(:, repo))+mean(HPB6(:, repo)));
+    if (isnan(mean(HUB6(:, repo))/(mean(HUB6(:, repo))+mean(HPB6(:, repo)))))
+        huproc_up(repo, 6) = 0;
+    end
+    buproc_up(repo, 1) = mean(BUB1(:, repo))/(mean(BUB1(:, repo))+mean(BPB1(:, repo)));
+    if (isnan(mean(BUB1(:, repo))/(mean(BUB1(:, repo))+mean(BPB1(:, repo)))))
+        buproc_up(repo, 1) = 0;
+    end
+    buproc_up(repo, 2) = mean(BUB2(:, repo))/(mean(BUB2(:, repo))+mean(BPB2(:, repo)));
+    if (isnan(mean(BUB2(:, repo))/(mean(BUB2(:, repo))+mean(BPB2(:, repo)))))
+        buproc_up(repo, 2) = 0;
+    end
+    buproc_up(repo, 3) = mean(BUB3(:, repo))/(mean(BUB3(:, repo))+mean(BPB3(:, repo)));
+    if (isnan(mean(BUB3(:, repo))/(mean(BUB3(:, repo))+mean(BPB3(:, repo)))))
+        buproc_up(repo, 3) = 0;
+    end
+    buproc_up(repo, 4) = mean(BUB4(:, repo))/(mean(BUB4(:, repo))+mean(BPB4(:, repo)));
+    if (isnan(mean(BUB4(:, repo))/(mean(BUB4(:, repo))+mean(BPB4(:, repo)))))
+        buproc_up(repo, 4) = 0;
+    end
+    buproc_up(repo, 5) = mean(BUB5(:, repo))/(mean(BUB5(:, repo))+mean(BPB5(:, repo)));
+    if (isnan(mean(BUB5(:, repo))/(mean(BUB5(:, repo))+mean(BPB5(:, repo)))))
+        buproc_up(repo, 5) = 0;
+    end
+    buproc_up(repo, 6) =  mean(BUB6(:, repo))/(mean(BUB6(:, repo))+mean(BPB6(:, repo)));
+    if (isnan(mean(BUB6(:, repo))/(mean(BUB6(:, repo))+mean(BPB6(:, repo)))))
+        buproc_up(repo, 6) = 0;
+    end
+    puproc_up(repo, 1) = mean(PUB1(:, repo))/(mean(PUB1(:, repo))+mean(PPB1(:, repo)));
+    if (isnan(mean(PUB1(:, repo))/(mean(PUB1(:, repo))+mean(PPB1(:, repo)))))
+        puproc_up(repo, 1) = 0;
+    end
+    puproc_up(repo, 2) = mean(PUB2(:, repo))/(mean(PUB2(:, repo))+mean(PPB2(:, repo)));
+    if (isnan(mean(PUB2(:, repo))/(mean(PUB2(:, repo))+mean(PPB2(:, repo)))))
+        puproc_up(repo, 2) = 0;
+    end
+    puproc_up(repo, 3) = mean(PUB3(:, repo))/(mean(PUB3(:, repo))+mean(PPB3(:, repo)));
+    if (isnan(mean(PUB3(:, repo))/(mean(PUB3(:, repo))+mean(PPB3(:, repo)))))
+        puproc_up(repo, 3) = 0;
+    end
+    puproc_up(repo, 4) = mean(PUB4(:, repo))/(mean(PUB4(:, repo))+mean(PPB4(:, repo)));
+    if (isnan(mean(PUB4(:, repo))/(mean(PUB4(:, repo))+mean(PPB4(:, repo)))))
+        puproc_up(repo, 4) = 0;
+    end
+    puproc_up(repo, 5) = mean(PUB5(:, repo))/(mean(PUB5(:, repo))+mean(PPB5(:, repo)));
+    if (isnan(mean(PUB5(:, repo))/(mean(PUB5(:, repo))+mean(PPB5(:, repo)))))
+        puproc_up(repo, 5) = 0;
+    end
+    puproc_up(repo, 6) =  mean(PUB6(:, repo))/(mean(PUB6(:, repo))+mean(PPB6(:, repo)));
+    if (isnan(mean(PUB6(:, repo))/(mean(PUB6(:, repo))+mean(PPB6(:, repo)))))
+        puproc_up(repo, 6) = 0;
     end
     
     
@@ -414,14 +512,14 @@ figure
 
 % subplot(2,1,1);
 % yyaxis left
-bar([mean(oproc(:,1)), mean(ouproc(:,1)), mean(oproc(:,2)), mean(ouproc(:,2)), mean(oproc(:,3)), mean(ouproc(:,3)), mean(oproc(:,4)), mean(ouproc(:,4)), mean(oproc(:,5)), mean(ouproc(:,5)), mean(oproc(:,6)), mean(ouproc(:,6));
-    mean(hproc(:,1)), mean(huproc(:,1)), mean(hproc(:,2)), mean(huproc(:,2)), mean(hproc(:,3)), mean(huproc(:,3)), mean(hproc(:,4)), mean(huproc(:,4)), mean(hproc(:,5)), mean(huproc(:,5)), mean(hproc(:,6)), mean(huproc(:,6));
-    mean(bproc(:,1)), mean(buproc(:,1)), mean(bproc(:,2)), mean(buproc(:,2)), mean(bproc(:,3)), mean(buproc(:,3)), mean(bproc(:,4)), mean(buproc(:,4)), mean(bproc(:,5)), mean(buproc(:,5)), mean(bproc(:,6)), mean(buproc(:,6));
-    mean(pproc(:,1)), mean(puproc(:,1)), mean(pproc(:,2)), mean(puproc(:,2)), mean(pproc(:,3)), mean(puproc(:,3)), mean(pproc(:,4)), mean(puproc(:,4)), mean(pproc(:,5)), mean(puproc(:,5)), mean(pproc(:,6)), mean(puproc(:,6))]);
+bar([mean(oproc_up(:,1)), mean(ouproc_up(:,1)), mean(oproc_up(:,2)), mean(ouproc_up(:,2)), mean(oproc_up(:,3)), mean(ouproc_up(:,3)), mean(oproc_up(:,4)), mean(ouproc_up(:,4)), mean(oproc_up(:,5)), mean(ouproc_up(:,5)), mean(oproc_up(:,6)), mean(ouproc_up(:,6));
+    mean(hproc_up(:,1)), mean(huproc_up(:,1)), mean(hproc_up(:,2)), mean(huproc_up(:,2)), mean(hproc_up(:,3)), mean(huproc_up(:,3)), mean(hproc_up(:,4)), mean(huproc_up(:,4)), mean(hproc_up(:,5)), mean(huproc_up(:,5)), mean(hproc_up(:,6)), mean(huproc_up(:,6));
+    mean(bproc_up(:,1)), mean(buproc_up(:,1)), mean(bproc_up(:,2)), mean(buproc_up(:,2)), mean(bproc_up(:,3)), mean(buproc_up(:,3)), mean(bproc_up(:,4)), mean(buproc_up(:,4)), mean(bproc_up(:,5)), mean(buproc_up(:,5)), mean(bproc_up(:,6)), mean(buproc_up(:,6));
+    mean(pproc_up(:,1)), mean(puproc_up(:,1)), mean(pproc_up(:,2)), mean(puproc_up(:,2)), mean(pproc_up(:,3)), mean(puproc_up(:,3)), mean(pproc_up(:,4)), mean(puproc_up(:,4)), mean(pproc_up(:,5)), mean(puproc_up(:,5)), mean(pproc_up(:,6)), mean(puproc_up(:,6))]);
 % title('Processing threads','fontsize',16)
 xlabel('Scenario Number','fontsize',12) 
 ylabel('Processed/UnProcessed Messages BW (B/s)','fontsize',12)
-ylim([0 15e5]);
+% ylim([0 15e5]);
 
 
 OPF1 = dlmread('officeIoT/RPFR1', ' ', 0, 1);
